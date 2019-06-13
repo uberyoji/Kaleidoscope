4d43688fb5b67c413b2f8f15af26b79503936926
uberyoji <jimmy.beaubien@gmail.com>
2019-06-12 20:09:57 -0400

    Almost done!
    Not sure what to add more.

--- Assets/Kaleidoscope.cs
+++ Assets/Kaleidoscope.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
+using UnityEngine.Networking;
 
 public class Kaleidoscope : MonoBehaviour
 {
@@ -8,10 +10,45 @@ public class Kaleidoscope : MonoBehaviour
     public float SliceLength = 1f;
 
     public float SliceTextureAngle = 45f;
-    
+    public float ScrollSpeed = 1f;
+    public float RotationSpeed = 1f;
+
+    private MeshRenderer R;
+
+    private int AngleId = 0;
+    private int ScrollId = 0;
+
+    public string Url = "";
+
+    IEnumerator DownloadImage(string MediaUrl)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+            Debug.Log(request.error);
+        else
+        {
+            
+            R.material.mainTexture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        R = GetComponent<MeshRenderer>();
+        AngleId = Shader.PropertyToID("_Angle");
+        ScrollId = Shader.PropertyToID("_Scroll");
+
+        SliceCount = URLParameters.GetSearchParameters().GetInt("slicecount", SliceCount);
+        ScrollSpeed = (float)URLParameters.GetSearchParameters().GetDouble("scrollspeed", ScrollSpeed);
+        RotationSpeed = (float)URLParameters.GetSearchParameters().GetDouble("rotationspeed", RotationSpeed);
+
+        if (Url != "" || URLParameters.GetSearchParameters().TryGetValue("url",out Url) )
+        {
+            StartCoroutine(DownloadImage(UnityWebRequest.UnEscapeURL(Url)));
+        }       
+
         SliceTextureAngle = 2 * Mathf.PI / SliceCount;
 
         GetComponent<MeshFilter>().sharedMesh = GenerateMesh();
@@ -20,7 +57,8 @@ public class Kaleidoscope : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        R.material.SetFloat(AngleId, Time.time * RotationSpeed);
+        R.material.SetFloat(ScrollId, Time.time * ScrollSpeed);
     }
 
     Mesh GenerateMesh()
@@ -29,29 +67,30 @@ public class Kaleidoscope : MonoBehaviour
         Vector3[] V = new Vector3[VertexCount];
         Vector2[] T = new Vector2[VertexCount];
         int[] I = new int[3 * SliceCount];
-
-
+        
         V[0] = new Vector3(0f, 0f, 0f);
         T[0] = new Vector3(0f, 0f);
 
         float a = 0f;
         float da = 2 * Mathf.PI / SliceCount;
 
+        Vector2[] TC = new Vector2[2] { new Vector2(0f, 1f), new Vector2(Mathf.Sin(SliceTextureAngle), Mathf.Cos(SliceTextureAngle))  };
+
         for(int i=0;i<SliceCount; i++)
         {
-            V[i + 1] = new Vector3(Mathf.Cos(a) * SliceLength, Mathf.Sin(a) * SliceLength, 0f);
-            T[i + 1] = new Vector2(0f, 1f);
+            V[2*i + 1] = new Vector3(Mathf.Cos(a) * SliceLength, Mathf.Sin(a) * SliceLength, 0f);
+            T[2*i + 1] = TC[i%2];
 
             a += da;
 
-            V[i + 2] = new Vector3(Mathf.Cos(a) * SliceLength, Mathf.Sin(a) * SliceLength, 0f);
-            T[i + 2] = new Vector2(Mathf.Cos(SliceTextureAngle), Mathf.Sin(SliceTextureAngle));
+            V[2*i + 2] = new Vector3(Mathf.Cos(a) * SliceLength, Mathf.Sin(a) * SliceLength, 0f);
+            T[2*i + 2] = TC[(i+1)%2];
         }
 
         for( int s=0;s<SliceCount;s++)
         {
             I[3*s] = 0;
-            I[3*s+1] = 2*s;
+            I[3*s+1] = 2*s+2;
             I[3*s+2] = 2*s+1;
         }
         
@@ -63,4 +102,15 @@ public class Kaleidoscope : MonoBehaviour
         };
         return M;
     }
+
+    /*
+    void ProcessUrl()
+    {
+        int pm = Application.absoluteURL.IndexOf("?");
+        if (pm != -1)
+        {
+            sceneName = Application.absoluteURL.Split("?"[0])[1];
+        }
+    }
+    */
 }
---
+++ Assets/Kaleidoscope.shader
@@ -0,0 +1,68 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Custom/Kaleidoscope"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_Pivot("Pivot", Vector) = (0.5,0.5,0,0)
+		_Angle("Angle", Range(-5.0,  5.0)) = 0.0
+		_Scroll("Scroll", Range(0.0,  1.0)) = 0.0
+	}
+		SubShader
+		{
+			Tags { "RenderType" = "Opaque" }
+
+			Pass
+			{
+				CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				#include "UnityCG.cginc"
+
+				struct v2f {
+					float4 pos : SV_POSITION;
+					float2 uv : TEXCOORD0;
+				};
+
+				float _Angle;
+				float _Scroll;
+				float4 _Pivot;
+
+				v2f vert(appdata_base v)
+				{
+					v2f o;
+					o.pos = UnityObjectToClipPos(v.vertex);
+
+					// Pivot
+					// float2 pivot = float2(0.5, 0.5);
+					// Rotation Matrix
+					float cosAngle = cos(_Angle);
+					float sinAngle = sin(_Angle);
+					float2x2 rot = float2x2(cosAngle, -sinAngle, sinAngle, cosAngle);
+
+					// Rotation consedering pivot
+					float2 uv = v.texcoord.xy;
+					
+					uv.y += _Scroll;
+					uv -= _Pivot.xy;
+					uv.y -= _Scroll;
+					o.uv = mul(rot, uv);
+					o.uv.y += _Scroll;
+					o.uv += _Pivot.xy;
+
+					return o;
+				}
+
+				sampler2D _MainTex;
+
+				fixed4 frag(v2f i) : SV_Target
+				{
+					// Texel sampling
+					return tex2D(_MainTex, i.uv);
+				}
+
+				ENDCG
+			}
+		}
+}
